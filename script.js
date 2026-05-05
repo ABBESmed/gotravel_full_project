103cd767e09b89d650a4302f883d454af9836983
Mohammed Zoheir Abbes <mohammedzoheirabbes@gmail.com>
2026-05-05 11:55:01 +0200

    Fix mobile menu burger

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// select elements
 const burger = document.querySelector('.site-header__burger');
 const menu = document.querySelector('.site-header__container');
 
-// when clicking burger
 burger.addEventListener('click', () => {
-    menu.classList.toggle('open');
+  menu.classList.toggle('open');
 });
